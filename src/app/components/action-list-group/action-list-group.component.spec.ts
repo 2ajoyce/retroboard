45cbcc7246f8cc2990eb3995712226c27b5307e3
Aaron Joyce <2ajoyce@gmail.com>
2018-08-07 16:44:29 -0500

    Move the action-list-group template into the ts file and test the ts file

--- src/app/components/action-list-group/action-list-group.component.spec.ts
+++ src/app/components/action-list-group/action-list-group.component.spec.ts
@@ -1,6 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ActionListGroupComponent } from './action-list-group.component';
+import {ActionListGroupComponent} from './action-list-group.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {Action} from '../../models/action';
+import {By} from '@angular/platform-browser';
 
 describe('ActionListGroupComponent', () => {
   let component: ActionListGroupComponent;
@@ -8,9 +11,10 @@ describe('ActionListGroupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActionListGroupComponent ]
+      declarations: [ActionListGroupComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +25,34 @@ describe('ActionListGroupComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when rendering', () => {
+    describe('Current app-action-list', () => {
+      it('should bind properties', () => {
+        const list = [<Action>{description: 'A Current Action'}];
+        component.current = list;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('app-action-list#current')).properties.list).toEqual(list);
+      });
+    });
+
+    describe('Old app-action-list', () => {
+      it('should bind properties', () => {
+        const list = [<Action>{description: 'An Old Action'}];
+        component.old = list;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('app-action-list#old')).properties.list).toEqual(list);
+      });
+    });
+
+    describe('Older app-action-list', () => {
+      it('should bind properties', () => {
+        const list = [<Action>{description: 'An Older Action'}];
+        component.older = list;
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('app-action-list#older')).properties.list).toEqual(list);
+      });
+    });
   });
 });
--- src/app/components/action-list-group/action-list-group.component.ts
+++ src/app/components/action-list-group/action-list-group.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Action } from '../../models/action';
-import {MatInputModule} from '@angular/material';
+import {Component, Input, OnInit} from '@angular/core';
+import {Action} from '../../models/action';
 
 @Component({
   selector: 'app-action-list-group',
-  templateUrl: './action-list-group.component.html',
-  styleUrls: ['./action-list-group.component.scss']
+  styleUrls: ['./action-list-group.component.scss'],
+  template: `
+    <h4>Action Items</h4>
+    <form class="action-form">
+      <input placeholder="Add an action item">
+    </form>
+    <div id="action-lists">
+      <app-action-list id="current" [list]="current"></app-action-list>
+      <app-action-list id="old" [list]="old"></app-action-list>
+      <app-action-list id="older" [list]="older"></app-action-list>
+    </div>
+
+  `,
 })
 
 export class ActionListGroupComponent implements OnInit {
